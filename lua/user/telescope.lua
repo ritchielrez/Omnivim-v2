160a73fbc1dbe81f840843a00197262bf696ea15
RitchielRez <ritchielrez@gmail.com>
2022-07-04 23:54:14 +0600

    feat: initial commit

---
+++ lua/user/telescope.lua
@@ -0,0 +1,30 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+  return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup {
+  defaults = {
+
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "truncate" },
+    winblend = 0,
+    border = {},
+    borderchars = { "", "", "", "", "", "", "", "" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+    
+    color_devicons = true,
+
+    mappings = {
+      i = {
+        ["<Down>"] = actions.cycle_history_next,
+        ["<Up>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+    },
+  },
+}
